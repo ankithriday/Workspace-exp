e334b47368f8b2b0034c74a29d9916fd9207031a
ankithriday <36642123+ankithriday@users.noreply.github.com>
2018-12-19 12:46:01 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 rm ~/difference.txt
 
 diff -x '.*' -rq . ~/Archive > ~/difference.txt
-cat difference.txt | awk '{print $2}' > file.txt
+cat ~/difference.txt | awk '{print $2}' > file.txt
 
 cp -r . ~/Archive
 
