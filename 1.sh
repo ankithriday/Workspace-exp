7e821cfc36e24ccc67402e7b3614487adadcb151
ankithriday <36642123+ankithriday@users.noreply.github.com>
2018-12-19 12:55:02 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,22 @@
+#######################
+####Ankit Hriday#######
+#######################
+
+# Needed not needed depending on availability
 rm ~/difference.txt
-
+# save differences ignores any hidden files between current directory and archive
 diff -x '.*' -rq . ~/Archive > ~/difference.txt
-cat ~/difference.txt | awk '{print $2}' > ~/file.txt
+# Only Saves the second line for scp(source copy)
+$file=difference.txt
+if [ ! -f "$file" ]
+then
+   echo "Difference File doesn't exist"
+else
+   echo "a is not equal to b"
+   cat ~/difference.txt | awk '{print $2}' > ~/file.txt
 
+fi
+# copy the current directory to archive
 cp -r . ~/Archive
-
-cat ~/maindiff.txt
+# Just print the output
+cat ~/file.txt
